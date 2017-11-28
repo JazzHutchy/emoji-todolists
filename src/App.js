d5d0dbd7b0b491dae95ae6600a6eed9c4af2b3d1
Jared Hutchinson <jared.hutchinson99@gmail.com>
2017-11-28 12:12:00 +1100

    Add items.map

--- src/App.js
+++ src/App.js
@@ -3,18 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
+const items = [
+  { description: 'First', completed: true },
+  { description: 'Second', completed: true },
+  { description: 'Third', completed: false },
+]
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
 
         <br />
 
-        <div className="App">
-          <TodoItem description='First' completed={true} />
+        <div className="App2">
+          {
+            items.map((item, index) => (
+              <TodoItem
+                key={index}
+                description={item.description}
+                completed={item.completed}
+              />
+            ))
+          }
+          <TodoItem description='First' completed />
           <TodoItem description='Second' completed />
           <TodoItem description='Third' />
         </div>
