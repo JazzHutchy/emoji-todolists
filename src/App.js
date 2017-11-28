75addcf8679f3bee9558a4628c8cc7ba02e9ca21
Jared Hutchinson <jared.hutchinson99@gmail.com>
2017-11-28 14:10:15 +1100

    Add completion checks and toggling

--- src/App.js
+++ src/App.js
@@ -3,19 +3,70 @@ import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
-const items = [
-  { description: 'First', completed: true },
-  { description: 'Second', completed: true },
-  { description: 'Third', completed: false },
-]
 
+// UI (view)
 class App extends Component {
+  // Data (model)
+  state = {
+    items: [
+      { description: 'First', completed: true },
+      { description: 'Second', completed: true },
+      { description: 'Third', completed: false },
+      { description: 'Fourth', completed: true },
+    ]
+  }
+
+  onToggleItemAtIndex = (index) => {
+    // Change our app's state
+    this.setState((prevState) => {
+      // Get current items
+      const beforeItems = prevState.items
+
+      const afterItems = beforeItems.map((item, currentIndex) => {
+        // When we have found the index of the item to change
+        if (currentIndex === index) {
+          return {
+            ...item, // Copy key-value pairs from existing item
+            completed: !item.completed // Override the `completed` to be the opposite of before
+          }
+        }
+        // No changes to make to this particular item
+        else {
+          return item
+        }
+      })
+      // // Find the item with the particular `index`
+      // const item = afterItems[index]
+      // // Toggle completed: f -> t, t -> f
+      // item.completed = !item.completed
+
+      // // Return the changes we have
+      return {
+        items: afterItems
+      }
+    })
+  }
+
   render() {
+    const items = this.state.items
+
+    const total = items.length
+
+    let totalCompleted = 0
+    let totalIncomplete = 0
+
+    items.forEach((item) => {
+      if (item.completed) {
+        totalCompleted += 1
+      }
+      else {
+        totalIncomplete += 1
+      }
+    })
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={logo} className="App-logo" alt="logo" />
           <img src={logo} className="App-logo" alt="logo" />
           <img src={logo} className="App-logo" alt="logo" />
           <img src={logo} className="App-logo" alt="logo" />
@@ -24,22 +75,36 @@ class App extends Component {
         <br />
 
         <div className="App2">
+          <dl>
+            <dt>Total</dt>
+            <dd>{total}</dd>
+
+            <dt>Total Completed</dt>
+            <dd>{totalCompleted}</dd>
+
+            <dt>Total Incomplete</dt>
+            <dd>{totalIncomplete}</dd>
+          </dl>
           {
             items.map((item, index) => (
               <TodoItem
                 key={index}
                 description={item.description}
                 completed={item.completed}
+                onToggleCompleted={
+                  () => {
+                    console.log('TodoItem onToggleCompleted received', index)
+                    this.onToggleItemAtIndex(index)
+                  }
+                }
               />
             ))
           }
-          <TodoItem description='First' completed />
-          <TodoItem description='Second' completed />
-          <TodoItem description='Third' />
         </div>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,11 +2,23 @@ import React from 'react'
 
 function TodoItem({
   description,
-  completed = false // If not passed, default to 'false'
+  completed = false, // If not passed, default to 'false'
+  onToggleCompleted
 }) {
   return (
     <label>
-      <input type='checkbox' />
+      <input
+        type='checkbox'
+        checked={completed}
+        onChange={
+          (event) => { // Event Listener
+            console.log('Clicked checkbox', description)
+            // When user clicks checkbox
+            // Notify the powers above that the concept of completed has changed
+            onToggleCompleted()
+          }
+        }
+      />
       {description}
     </label>
   )
